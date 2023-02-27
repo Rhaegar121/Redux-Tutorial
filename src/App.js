0f44e854263282d748ec5d85c8919a9f8bed8503
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-27 19:49:42 +0630

    add calculateTotals in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import { useEffect } from 'react';
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals } from './features/cart/cartSlice';
 
 function App() {
+  const { cartItems } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
     <main>
       <Navbar />
       <CartContainer />
     </main>
-  )
+  );
 }
 export default App;
