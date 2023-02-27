2a653c24b1ef25fa6a189ff5e2a637be6e88291c
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-02-27 19:55:00 +0630

    add modalSlice.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 import { useSelector, useDispatch } from 'react-redux';
 import { calculateTotals } from './features/cart/cartSlice';
+import Modal from './components/Modal';
 
 function App() {
   const { cartItems } = useSelector((state) => state.cart);
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <main>
+      <Modal />
       <Navbar />
       <CartContainer />
     </main>
